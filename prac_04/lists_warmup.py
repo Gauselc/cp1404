0845a15befc1c9f272916bce24914f3d6bbc1f7e
Gauselc <christopher.gausel@my.jcu.edu.au>
2021-08-25 10:43:14 +1000

    Add prac_04

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+numbers[0] = 3
+numbers[-1] = 2
+numbers[3] = 1
+numbers[:-1] = 3, 1, 4, 1, 5, 9
+numbers[3:4] = 1, 5
+5 in numbers = True
+7 in numbers = False
+"3" in numbers = False
+numbers + [6, 5, 3] = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+"""
+
+# Q.1
+# numbers[0] = "ten"
+
+# Q.2
+# numbers[-1] = 1
+
+# Q.3
+# print(numbers[2:])
+
+# Q.3
+# 9 in numbers
